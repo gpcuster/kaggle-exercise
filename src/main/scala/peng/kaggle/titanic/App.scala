ce59e7022b267a0ae41b915c5e48e2bc4f46050b
Peng Guo <gpcuster@gmail.com>
2017-04-28 14:07:06 +1200

    add basic logic for titanic

--- src/main/scala/peng/kaggle/titanic/App.scala
+++ src/main/scala/peng/kaggle/titanic/App.scala
@@ -1,7 +1,102 @@
 package peng.kaggle.titanic
 
+import org.apache.spark.sql.types._
+import peng.kaggle.util.SparkUtils
+import org.apache.spark.sql.functions._
+import org.apache.spark.ml.feature.VectorAssembler
+
+import org.apache.spark.ml.classification.{LogisticRegression, DecisionTreeClassifier, NaiveBayes, RandomForestClassifier}
+import org.apache.spark.ml.{Estimator, Transformer}
+
 object App {
   def main(args: Array[String]): Unit = {
-    println("Hello, world!")
+    val inputPath = "src/main/resources/data/titanic/train.csv"
+
+
+    //guessed,difficultyLevel,confidenceAverage,choiceChanged,differentChoices,underExpectedTime
+    val customSchema = StructType(Array(
+      StructField("PassengerId", IntegerType, false),
+      StructField("Survived", IntegerType, false),
+      StructField("Pclass", IntegerType, false),
+      StructField("Name", StringType, false),
+      StructField("Sex", StringType, false),
+      StructField("Age", DoubleType, false),
+      StructField("SibSp", IntegerType, false),
+      StructField("Parch", IntegerType, false),
+      StructField("Ticket", StringType, false),
+      StructField("Fare", DoubleType, false),
+      StructField("Cabin", StringType, false),
+      StructField("Embarked", StringType, false)
+    )
+    )
+
+    val inputDF = SparkUtils.getSpark().read.format("com.databricks.spark.csv")
+      .option("header", "true") // Use first line of all files as header
+      .schema(customSchema)
+      .load(inputPath)
+
+    inputDF.createOrReplaceTempView("inputTable")
+
+    inputDF.show(10, false)
+
+    SparkUtils.sql("select * from inputTable where survived = 1")
+
+    val convertSex = udf { sex: String => sex == "male"}
+
+    val convertAge = udf { age: Double => age match {
+      case d:Double => d
+      case _ => 0
+    } }
+
+    val convertedInpuDF = inputDF.withColumn("Sex2", convertSex(col("Sex"))).withColumn("Age2", convertSex(col("Age")))
+
+    convertedInpuDF.show
+
+    val Array(training, test) = inputDF.randomSplit(Array(0.7, 0.3), seed = 12345)
+
+
+    val assembler = new VectorAssembler()
+      .setInputCols(Array(
+        "Pclass",
+        //"Sex",
+        "Age",
+        "SibSp",
+        "Parch",
+        //"Ticket",
+        "Fare"
+        //"Cabin",
+        //"Embarked"
+      ))
+      .setOutputCol("features")
+
+    val trainingWithFeatures = assembler.transform(training)
+    val testWithFeatures = assembler.transform(test)
+
+    trainingWithFeatures.show(10, false)
+
+    val alg = "lr"
+
+    val estimator = if (alg == "lr") {
+      new LogisticRegression()
+        .setMaxIter(100)
+        .setRegParam(0.000001)
+        .setLabelCol("Survived")
+        .setFeaturesCol("features")
+    } else if (alg == "dt") {
+      new DecisionTreeClassifier()
+        .setLabelCol("Survived")
+        .setFeaturesCol("features")
+    } else if (alg == "rf") {
+      new RandomForestClassifier()
+        .setLabelCol("Survived")
+        .setFeaturesCol("features")
+        .setNumTrees(10)
+    } else if (alg == "nb") {
+      new NaiveBayes()
+        .setLabelCol("Survived")
+        .setFeaturesCol("features")
+    }
+
+    val model = estimator.asInstanceOf[Estimator[_]].fit(trainingWithFeatures).asInstanceOf[Transformer]
   }
 }
---
+++ src/main/scala/peng/kaggle/util/SparkUtils.scala
@@ -0,0 +1,17 @@
+package peng.kaggle.util
+
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+
+object SparkUtils {
+  def getSpark() = {
+    val conf = new SparkConf().setMaster("local[2]")
+    val builder = SparkSession.builder.config(conf)
+    val spark = builder.getOrCreate()
+    spark
+  }
+
+  def sql(sql: String) = {
+    getSpark().sql(sql).collect().foreach(println)
+  }
+}
